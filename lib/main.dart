e960746a205c8e49b41317f412bbb4f3e11f6ff5
omrobbie <putu.roby@yahoo.com>
2018-03-20 07:25:05 +0800

    tampilkan data, masih error di getSwData()

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,74 @@ class StarWarsData extends StatefulWidget {
 }
 
 class _StarWarsDataState extends State<StarWarsData> {
-  final String url = "https://swapi.co/api/starships";
+  final String url = 'https://swapi.co/api/starships';
   List data;
 
   Future<String> getSwData() async {
     var res = await http.get(
       Uri.encodeFull(url),
-      headers: {"Accept": "application/json"},
+      headers: {'Accept': 'application/json'},
     );
 
     setState(() {
       var resBody = json.decode(res.body);
-      data = resBody["results"];
+      data = resBody['results'];
     });
 
-    return "Success!";
+    return 'Success!';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Star Wars Starships"),
-        backgroundColor: Colors.green,
+        title: Text('Star Wars Starships'),
+        backgroundColor: Colors.yellow,
       ),
       body: ListView.builder(
         itemCount: data == null ? 0 : data.length,
         itemBuilder: (BuildContext context, int index) {
-
+          return new Container(
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Card(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        data[index]['name'],
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        data[index]['model'],
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
         },
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //this.getSwData();
   }
 }
